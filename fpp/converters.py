bf65de80921c548b02a0fb833d472f309379a16f
João Périco <jeperico@terra.com.br>
2024-07-09 17:58:20 -0300

    [PROJECT-01]: "Spreadsheets Automation"

--- fpp/converters.py
+++ fpp/converters.py
@@ -1,6 +1,8 @@
-def lbs_to_kgs(weight):
-    return weight * 0.45
+weight_1 = int(input("What's your weight? "))
 
+def lbs_to_kgs(weight):
+    new_weight = weight * 0.45
+    print(f"Your weight in kgs is {new_weight}")
 
 def kgs_to_lbs(weight):
     return weight / 0.45
@@ -8,3 +10,5 @@ def kgs_to_lbs(weight):
 
 def talk(message):
     print(message)
+
+lbs_to_kgs(weight_1)
